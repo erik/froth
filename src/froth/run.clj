a63cbbb3450094e06aee8e117e205dc3cbc88a05
boredomist <erik.price16@gmail.com>
2010-09-14 17:38:30 -0400

    Allow execution of files

--- src/froth/run.clj
+++ src/froth/run.clj
@@ -3,28 +3,38 @@
   (:import [java.io BufferedReader StringReader]
 	   [java.util Scanner]))
 
-;; REPL
 (def prompt "> ")
 
-(dictionary-defaults) 
+(dictionary-defaults)
 
-(println "froth version" *froth-version*)
-(println "Copyright (c) 2010, Erik Price")
-
-(print prompt)
-(flush)
-
-(swap! froth-reader (constantly (Scanner. (read-line))))
-
-(loop []
-  (try
-    (evaluate)
-    (catch Exception e (println (.getMessage e))))
+(defn repl []
+  "The froth REPL"
+  (println "froth version" *froth-version*)
+  (println "Copyright (c) 2010, Erik Price")
   
   (print prompt)
   (flush)
   
   (swap! froth-reader (constantly (Scanner. (read-line))))
   
-  (recur))
-  
+  (while true
+    (try
+      (evaluate)
+      (catch Exception e (println (.getMessage e))))
+    
+    (print prompt)
+    (flush)
+    
+    (swap! froth-reader (constantly (Scanner. (read-line))))))
+
+(defn read-file [args]
+  "Reads files named by args in order"
+  (doseq [file (seq args)]
+    (try
+      (swap! froth-reader (constantly (Scanner. (slurp file))))
+      (evaluate)
+      (catch Exception e (println (.getMessage e)) (System/exit 1)))))
+
+(if (seq *command-line-args*)
+  (read-file *command-line-args*)
+  (repl))
